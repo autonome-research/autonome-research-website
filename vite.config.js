4fd1063beabecc7e48476ea7ae65b2abf1f33f44
Code4me2 <reichmanw@gmail.com>
2026-07-10 13:30:06 -0700

    Add standalone content pages

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog/index.html'),
+        research: resolve(__dirname, 'research/index.html'),
+        projects: resolve(__dirname, 'projects/index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+      },
+    },
+  },
+});
